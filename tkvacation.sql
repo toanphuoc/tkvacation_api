82e28556cbcf1484a9d68953d8246c948fc52960
ToanNguyenP <toan.nguyenp@s3corp.com.vn>
2017-06-02 17:54:20 +0700

    asss

--- tkvacation.sql
+++ tkvacation.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: tkvacation
+-- Host: 127.0.0.1    Database: tkvacation
 -- ------------------------------------------------------
--- Server version	5.7.16-log
+-- Server version	5.6.23-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,6 +41,34 @@ INSERT INTO `destinations` VALUES (1,'img/destination_1.jpg','Ha Long'),(2,'img/
 UNLOCK TABLES;
 
 --
+-- Table structure for table `itinerary`
+--
+
+DROP TABLE IF EXISTS `itinerary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `itinerary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text,
+  `tour_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ITINERARY_TOUR_idx` (`tour_id`),
+  CONSTRAINT `FK_ITINERARY_TOUR` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itinerary`
+--
+
+LOCK TABLES `itinerary` WRITE;
+/*!40000 ALTER TABLE `itinerary` DISABLE KEYS */;
+INSERT INTO `itinerary` VALUES (1,' Arrive in Zürich, Switzerland','Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus.',1),(2,' Zürich–Biel/Bienne–Neuchâtel–Geneva','Enjoy an orientation walk of Zurich’s OLD TOWN, Switzerland’s center of banking and commerce. Then, leave Zurich and start your Swiss adventure. You’ll quickly discover that Switzerland isn’t just home to the Alps, but also to some of the most beautiful lakes. First, stop at the foot of the Jura Mountains in the picturesque town of Biel, known as Bienne by French-speaking Swiss, famous for watch-making, and explore the historical center. Next, enjoy a scenic drive to lakeside Neuchâtel, dominated by the medieval cathedral and castle. Time to stroll along the lake promenade before continuing to stunning Geneva, the second-largest city in Switzerland, with its fantastic lakeside location and breathtaking panoramas of the Alps.',1);
+/*!40000 ALTER TABLE `itinerary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tours`
 --
 
@@ -58,10 +86,11 @@ CREATE TABLE `tours` (
   `booking` int(11) DEFAULT '0',
   `price_vnd` decimal(10,2) DEFAULT NULL,
   `img` varchar(45) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_TOUR_DES_idx` (`destination_id`),
   CONSTRAINT `FK_TOURS_DES` FOREIGN KEY (`destination_id`) REFERENCES `destinations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,9 +99,13 @@ CREATE TABLE `tours` (
 
 LOCK TABLES `tours` WRITE;
 /*!40000 ALTER TABLE `tours` DISABLE KEYS */;
-INSERT INTO `tours` VALUES (1,'Mekong 1 day, Cai Lay','1 Day','2 Days','Enjoy full day at the local house and join their activities, a wish of so many people live at the busy city such at Saigon. This is a beautiful destination with less tourist.',200.00,1,0,5200000.00,'img/tour_1.jpg'),(2,'City full day','1 Day','1 Day','You will explore Saigon in deep 1 full day. Enjoy walking around the Chinese market and the park, have a talk with the guide at the local coffee shop.',100.00,1,0,2000000.00,'img/tour_2.jpg'),(4,'Ha Long','3213','eqwe','ded',100.00,2,0,NULL,NULL);
+INSERT INTO `tours` VALUES (1,'Mekong 1 day, Cai Lay','1 Day','June 6','Enjoy full day at the local house and join their activities, a wish of so many people live at the busy city such at Saigon. This is a beautiful destination with less tourist.',200.00,1,0,5200000.00,'img/tour_6.jpg','2017-06-01 00:00:00'),(2,'City full day','1 Day','June 6','You will explore Saigon in deep 1 full day. Enjoy walking around the Chinese market and the park, have a talk with the guide at the local coffee shop.',100.00,1,0,2000000.00,'img/tour_7.jpg','2017-06-02 00:00:00'),(4,'Ha Long','2 Days','June 6','You will explore Saigon in deep 1 full day. Enjoy walking around the Chinese market and the park, have a talk with the guide at the local coffee shop.',100.00,2,0,2400000.00,'img/tour_8.jpg','2017-06-02 00:00:00'),(5,'Hoi An','3 Days','June 8','You will explore Saigon in deep 1 full day. Enjoy walking around the Chinese market and the park, have a talk with the guide at the local coffee shop.',150.00,3,0,5000000.00,'img/tour_8.jpg','2017-06-02 00:00:00');
 /*!40000 ALTER TABLE `tours` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'tkvacation'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -83,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-02  0:14:03
+-- Dump completed on 2017-06-02 17:53:36
